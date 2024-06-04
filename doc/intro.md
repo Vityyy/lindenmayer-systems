fdd9b536f75301ad01dcaf7f5448d6d530f99acb
Camilo Sassone Irrazabal <csassone@fi.uba.ar>
2024-06-04 14:26:12 -0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to algo3-tp2-sistemasl
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/algo3_tp2_sistemasl/core_test.clj
@@ -0,0 +1,7 @@
+(ns algo3-tp2-sistemasl.core-test
+  (:require [clojure.test :refer :all]
+            [algo3-tp2-sistemasl.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
