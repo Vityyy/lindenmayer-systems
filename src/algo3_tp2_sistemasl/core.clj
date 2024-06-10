87692b3d13fa86f59f93b4c9362874603a02434c
Camilo Sassone Irrazabal <csassone@fi.uba.ar>
2024-06-09 21:42:39 -0300

    Cambios finales de versión 1
    Co-Authored-By: ColinaAndres <130932179+ColinaAndres@users.noreply.github.com>

--- src/algo3_tp2_sistemasl/core.clj
+++ src/algo3_tp2_sistemasl/core.clj
@@ -1,12 +1,11 @@
 (ns algo3-tp2-sistemasl.core
   (:require
     [clojure.string :as str]))
-
 (defn procesar-entrada [nombre-archivo]
   (str/split (slurp (str "resources/" nombre-archivo)) #"\n") )
 
 (defn nueva-tortuga [coord-x coord-y angulo pluma-abajo]
-  {:x coord-x :y coord-y :angulo-cons angulo :angulo-act angulo :pluma pluma-abajo})
+  {:x coord-x :y coord-y :angulo-cons angulo :angulo-act 90 :pluma pluma-abajo})
 
 (defn adelante [tortuga n]
   (let [angulo-radianes (Math/toRadians (get tortuga :angulo-act))
@@ -32,7 +31,8 @@
   (update tortuga :pluma true))
 
 (defn apilar-tortuga [tortuga pila]
-  (conj pila tortuga))
+  (conj pila (into {} tortuga)))
+
 (defn desapilar-tortuga [pila]
   (rest pila))
 
@@ -51,49 +51,52 @@
   (let [minimo-x (reduce min (concat (map :x1 historial) (map :x2 historial)))
         minimo-y (reduce min (concat (map :y1 historial) (map :y2 historial)))
         maximo-x (reduce max (concat (map :x1 historial) (map :x2 historial)))
-        maximo-y (reduce max (concat (map :y1 historial) (map :y2 historial)))]
-    (vector minimo-x minimo-y maximo-x maximo-y))
+        maximo-y (reduce max (concat (map :y1 historial) (map :y2 historial)))
+        ancho (- maximo-x minimo-x)
+        alto (- maximo-y minimo-y)
+        ]
+    (vector minimo-x maximo-y ancho alto))
   )
 
 (defn escribir-svg [historial nombre-archivo]
   (let [
-        mov (last historial)
-        linea (str "<line x1=\"" (:x1 mov) "\"" " y1=\"" (:y1 mov) "\"" " x2=\"" (:x2 mov) "\"" " y2=\"" (:y2 mov) "\"" " stroke-width=\"1\" stroke=\"black\" />")
+        mov (first historial)
+        linea (str "  <line x1=\"" (:x1 mov) "\" y1=\"" (-(:y1 mov)) "\" x2=\"" (:x2 mov) "\" y2=\"" (-(:y2 mov)) "\" stroke-width=\"1\" stroke=\"black\" />")
         rest (rest historial)
         ]
-    (spit nombre-archivo linea)
+    (spit nombre-archivo linea :append true)
     (if (not-empty rest)
-      (escribir-svg rest nombre-archivo))))
+      (recur rest nombre-archivo))))
 
 (defn escribir-svg-wrapper [historial nombre-archivo]
-  (let [limites(calcular-limites historial)]
-    (spit nombre-archivo (str "<svg viewBox=\"" (limites 0) "\"" (limites 1) "\"" (limites 2) "\"" (limites 3) "\"" "xmlns=\"http://www.w3.org/2000/svg\" />")))
-
+  (let [limites(calcular-limites historial)
+        viewbox (str (- (limites 0) 100) " " (- (- (limites 1)) 100) " " (+ (limites 2) 100) " " (+ (limites 3) 100))]
+    (spit nombre-archivo (str "<svg viewBox=\"" viewbox "\" xmlns=\"http://www.w3.org/2000/svg\">") :append true)
   (escribir-svg historial nombre-archivo)
-  (spit nombre-archivo "</svg>")
-  )
-(defn dibujar [pila expresion historial]
-  (let [
-        tortuga (first pila)
-        paso (str (first expresion))
-        tortuga-nueva (cond
-                        (or (= paso "F") (= paso "G")) (adelante tortuga 1)
-                        (or (= paso "f") (= paso "g")) (-> tortuga (pluma-arriba) (adelante 1) (pluma-abajo))
-                        (= paso "+") (derecha tortuga)
-                        (= paso "-") (izquierda tortuga)
-                        (= paso "|") (invertir tortuga))
-
-        nueva_pila (cond
-                     (= paso "[") (apilar-tortuga tortuga pila)
-                     (= paso "]") (desapilar-tortuga pila)
-                     (not (nil? tortuga-nueva)) (apilar-tortuga tortuga-nueva (desapilar-tortuga pila))
-                     :else pila)
-
-        historial-actualizado (detectar-movimiento paso tortuga tortuga-nueva historial)]
-    (if (not-empty expresion)
-      (dibujar nueva_pila (rest expresion) historial-actualizado)
-      historial-actualizado)
+  (spit nombre-archivo "</svg>" :append true)
   ))
+(defn dibujar [pila expresion historial]
+  (if (empty? expresion)
+    historial
+    (let [
+          tortuga (first pila)
+          paso (str (first expresion))
+          tortuga-nueva (cond
+                          (or (= paso "F") (= paso "G")) (adelante tortuga 15)
+                          (or (= paso "f") (= paso "g")) (-> tortuga (pluma-arriba) (adelante 15) (pluma-abajo))
+                          (= paso "+") (derecha tortuga)
+                          (= paso "-") (izquierda tortuga)
+                          (= paso "|") (invertir tortuga))
+
+          nueva_pila (cond
+                       (= paso "[") (apilar-tortuga tortuga pila)
+                       (= paso "]") (desapilar-tortuga pila)
+                       (not (nil? tortuga-nueva)) (apilar-tortuga tortuga-nueva (desapilar-tortuga pila))
+                       :else pila)
+
+          historial-actualizado (detectar-movimiento paso tortuga tortuga-nueva historial)]
+
+      (recur nueva_pila (rest expresion) historial-actualizado))))
 
 (defn wrap-tortuga [angulo expresion]
   (let [tortuga (nueva-tortuga 0 0 angulo true)
@@ -102,19 +105,19 @@
 
     (dibujar pila expresion historial)))
 
-(defn hallar-expr-aux [expresion-actual reglas]
+(defn hallar-expr-aux [expresion-actual reglas resultado]
   (if (empty? expresion-actual)
-    ""
+    resultado
     (let [elemento-actual (str (first expresion-actual))
           regla (get reglas (keyword elemento-actual) elemento-actual)]
 
-      (str regla (hallar-expr-aux (rest expresion-actual) reglas)))))
+      (recur (rest expresion-actual) reglas (str resultado regla)))))
 
 (defn hallar-expresion [expresion reglas iteraciones]
   (if (zero? iteraciones)
     expresion
-    (let [nueva-expresion (hallar-expr-aux expresion reglas)]
-      (hallar-expresion nueva-expresion reglas (dec iteraciones))))
+    (let [nueva-expresion (hallar-expr-aux expresion reglas "")]
+      (recur nueva-expresion reglas (dec iteraciones))))
   )
 
 (defn convertir-reglas-a-diccionario [reglas]
